9c459dab9d34fc60290c0228e41f5e879540944c
Lionel <fullstackbeaver@gmail.com>
2020-10-13 10:32:19 +0200

    séance du 13 octobre

--- js/composants/product.js
+++ js/composants/product.js
@@ -1,25 +1,64 @@
 class Product {
-    constructor(domTarget){
-
+    constructor(props, domTarget, container){
+      for (const [key, value] of Object.entries(props)){
+        this[key] = value;
+      }
+      this.showDetails = false;
+      this.DOM = document.createElement("li");
+      domTarget.appendChild(this.DOM);
+      window[container].components["product"+this._id] = this;
+      this.container = container;
+      this.render();
     }
 
 
-    template(){
+    resumeTemplate(){
         return `
         
         <figure>
-        <a class="aa-product-img" href="#"><img src="${this.products[i].imgURL}" alt="teddy_img"></a>
+        <a class="aa-product-img" href="#"><img src="${this.imageUrl}" alt="teddy_img"></a>
         <a class="aa-add-card-btn"href="checkout.html"><span class="fa fa-shopping-cart"></span>Ajouter au panier</a>
         <figcaption>
-           <h4 class="aa-product-title"><a href="#">Peluche ours marron</a></h4>
-           <span class="aa-product-price">12.90€</span>
+           <h4 class="aa-product-title"><a href="#">${this.name}</a></h4>
+           <span class="aa-product-price">${this.price/100}€</span>
         </figcaption>
      </figure>
      <div class="aa-product-hvr-content">
-        <a href="#" data-toggle2="tooltip" data-placement="top" title="Zoom" data-toggle="modal" data-target="#quick-view-modal"><span class="fa fa-search"></span></a>
+        <a href="#" data-toggle2="tooltip" data-placement="top" title="Zoom" data-toggle="modal" onclick="${this.container}.components.product${this._id}.changeView()"><span class="fa fa-search"></span></a>
      </div>        
         
         `;
     }
 
+    render(){
+      this.DOM.innerHTML = this.showDetails ? this.detailedTemplate() : this.resumeTemplate();
+    }
+
+    changeView(){
+      this.showDetails = !this.showDetails;
+      this.render();
+    }
+
+    detailedTemplate(){
+      return "-----";
+    }
+
+    die(){
+      this.DOM.parentNode.removeChild(this.DOM);
+      delete(window[this.container].components[["product"+this._id]]);
+    }
 }
+
+
+/*
+
+                                    <li>
+                                       <figure>
+                                          <a class="aa-product-img" href="#"><img src="http://localhost:3000/images/teddy_3.jpg" alt="teddy_3 img"></a>
+                                          <a class="aa-add-card-btn" href="#"><span class="fa fa-search"></span>Voir le produit</a>
+                                          <figcaption>
+                                             <h4 class="aa-product-title"><a href="#">Lenny and Carl</a></h4>
+                                          </figcaption>
+                                       </figure>
+                                    </li>
+                                    */
